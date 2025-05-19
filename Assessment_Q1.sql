ab7a6aef355192a520665c5f8b65be8787243564
chefo11 <fran6cool@gmail.com>
2025-05-19 13:27:25 +0100

    Readme

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -2,48 +2,53 @@ USE adashi_staging;
 -- Finding High-Value Customers with Multiple Products
 -- This SQL query identifies customers who have both a savings and an investment plan (cross-selling opportunity).
 
--- CTES for total deposit per customer
-WITH total_deposit_per_customer AS(
-    SELECT owner_id,
-              SUM(confirmed_amount) AS total_deposit
-    FROM savings_savingsaccount 
-    WHERE confirmed_amount > 0 
-    GROUP BY owner_id
-),
 
 -- CTES for customers with at least one funded savings plan
-funded_savings_customers AS (
-    SELECT s.owner_id,
-              COUNT(s.id) AS savings_count,
-              SUM(s.confirmed_amount) AS total_deposit
-              
-    FROM savings_savingsaccount s
-     JOIN plans_plan p ON s.plan_id = p.id
-     WHERE confirmed_amount > 0 AND p.is_regular_savings = 1
-    GROUP BY s.owner_id
-),
-
+WITH funded_savings_customers AS (
+  SELECT 
+    s.owner_id, 
+    COUNT(s.id) AS savings_count, 
+    SUM(s.confirmed_amount) AS total_savings 
+  FROM 
+    savings_savingsaccount s 
+    JOIN plans_plan p ON s.plan_id = p.id 
+  WHERE 
+    confirmed_amount > 0 
+    AND p.is_regular_savings = 1 
+  GROUP BY 
+    s.owner_id
+), 
 -- CTES for customers with at least one investment plan
 funded_investment_customers AS (
-    
-   SELECT s.owner_id,
-              COUNT(s.id) AS investment_count,
-              SUM(s.confirmed_amount) AS total_deposit
-              
-    FROM savings_savingsaccount s
-     JOIN plans_plan p ON s.plan_id = p.id
-     WHERE confirmed_amount > 0 AND p.is_a_fund = 1
-    GROUP BY s.owner_id
+  SELECT 
+    s.owner_id, 
+    COUNT(s.id) AS investment_count, 
+    SUM(s.confirmed_amount) AS total_investment 
+  FROM 
+    savings_savingsaccount s 
+    JOIN plans_plan p ON s.plan_id = p.id 
+  WHERE 
+    confirmed_amount > 0 
+    AND p.is_a_fund = 1 
+  GROUP BY 
+    s.owner_id
 )
 
--- Customer with at least one funded savings plan and one funded investment plan, sorted by total deposit
-SELECT u.id AS owner_id,
-           CONCAT(u.first_name, ' ', u.last_name) AS name,
-           s.savings_count,
-           i.investment_count,
-           ROUND((s.total_deposit + i.total_deposit), 2) AS total_deposit
- FROM users_customuser u
- JOIN funded_savings_customers s ON u.id = s.owner_id
-    JOIN funded_investment_customers i ON u.id = i.owner_id
-    JOIN total_deposit_per_customer t ON u.id = t.owner_id
-    ORDER BY savings_count ASC
+-- Final query: Customer with at least one funded savings plan and one funded investment plan, sorted by total deposit
+SELECT 
+  u.id AS owner_id, 
+  CONCAT(u.first_name, ' ', u.last_name) AS name, 
+  s.savings_count, 
+  i.investment_count, 
+  ROUND(
+    (
+      s.total_savings + i.total_investment
+    )/ 100, 
+    2
+  ) AS total_deposit 
+FROM 
+  users_customuser u 
+  JOIN funded_savings_customers s ON u.id = s.owner_id 
+  JOIN funded_investment_customers i ON u.id = i.owner_id 
+ORDER BY 
+  total_deposit DESC
--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 # DataAnalytics-Assessment
 DataAnalytics-Assessment
 
-## Assessment_Q1.sql
+## Assessment_Q1.sql: High-Value Customers with Multiple Products
 
 ### Task:
  To find customers with at least one funded savings plan AND one funded investment plan, sorted by total deposits
 
 ### Approach:
 
-- First all I used (CTES) Common Table Expression to evaluated customers with at least on savings account
+- First all I used (CTE) Common Table Expression to evaluated customers with at least one funded savings plan
     - Performed a JOIN (LEFT INNER JOIN) between the savings_savingsaccount table and plans_plan tables
-    - Used the WHERE clause to make use I filter to plans that are regular savings and made sure the confirmed amount is greater than 0
+    - Used the WHERE clause to make use I filter to plans that are regular savings (is_regular_savings = 1) and is funded(comfirmed_amount>0)
     - Used COUNT() count the number of savings plans for each customers
     - Used SUM() to sum the total confirmed amount for each customer
-
+- I evaluated customers with at least one funded investment plan using CTE
+    - Performed a JOIN (LEFT INNER JOIN) between the savings_savingsaccount table and plans_plan tables
+    - Used the WHERE clause to make use I filter to plans that are investment plans (is_funded=1) and is funded(comfirmed_amount>0)
+    - Used COUNT() count the number of savings plans for each customers
+    - Used SUM() to sum the total confirmed amount for each customer
+- I performed a JOIN between the funded_savings_customers and Investment customers to retain customers with both savings and investment plant
+- I sumed the total savings and total investment to get total deposit by each High Value Customers
 ### Challenges:
 
-- 
+- Most of the name column from the users_customuser table is null, I had to concatinate firstname and lastname column and then alias it name.
+- I had to ensure that the customers in the final table has both savings and investment plan, I used JOIN (LEFT INNER JOIN) to ensure this.
+
+## Assessment_Q2.sql: Transaction Frequency Analysis
+
+### To calculate the average number of transactions per customer per month and categorize them:
